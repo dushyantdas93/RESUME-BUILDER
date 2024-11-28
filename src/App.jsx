493cce510a598d4fab8eb5f43730a3118b437180
mernacademy2024 <mernacademy2024@gmail.com>
2024-11-28 16:35:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="bg-red-500">
+      App
+      <Button>asdasds</Button>
+    </div>
+  );
+};
+
+export default App;
